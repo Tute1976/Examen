d40e8bd3a2ce5475bcd791d0e40e5cf89583095b
Tuté: The Unsupported Total Evolution with closed accent <123418817+Tute1976@users.noreply.github.com>
2025-12-16 08:27:43 +0100

    .

---
+++ NetejaLogsCaptures.cmd
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+del D:\Privat\Programacio\Git\Examen\Professor\bin\Debug\Traces\RedisTraces.log > nul 2>&1
+del D:\Privat\Programacio\Git\Examen\Alumne\bin\Debug\Traces\RedisTraces.log > nul 2>&1
+del D:\Privat\Programacio\Git\Examen\Professor\bin\Debug\Captures\*.png > nul 2>&1
+del D:\Privat\Programacio\Git\Examen\Alumne\bin\Debug\Captures\*.png > nul 2>&1
+
+exit /B 0
